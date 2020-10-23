16685654271672f2571fd293a3498d75e8bfee6c
Roshan <roshannpradhan@gmail.com>
2020-10-23 17:39:55 -0400

    landmarks converging in wrong place, but seems reasonable-ish

--- 16833_HW2_EKF/code/EKF_slam/matlab/calcPre.m
+++ 16833_HW2_EKF/code/EKF_slam/matlab/calcPre.m
@@ -8,11 +8,10 @@ x_pre(3) = x(3) + alpha;
 P_pre = P;
 
 P_w = blkdiag(P(1:3, 1:3), control_cov);
-size(P_w)
 
-A_jac= [0, 0, -sin(x(3))*d, cos(x(3)), -sin(x(3)), 0;
-            0, 0, cos(x(3))*d, sin(x(3)), cos(x(3)), 0;
-            0, 0, 0, 0, 0, 1];
+A_jac= [1, 0, -sin(x(3))*d, cos(x(3)), -sin(x(3)), 0;
+            0, 1, cos(x(3))*d, sin(x(3)), cos(x(3)), 0;
+            0, 0, 1, 0, 0, 1];
 
 P_pre(1:3, 1:3) = A_jac * P_w * A_jac';
 
